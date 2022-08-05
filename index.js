a470cc18fc6d0569860a87515fa4b21f52f3594a
lucas <l.l.m.lalande@student.utwente.nl>
2022-08-05 18:51:43 +0200

    Worked on login, partially working. Left to style the login, call a proper get instead of simply using href on the login button . Style the login status and make a form to submit credentials. Session is set . started to use EJS for templating

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const mysql = require('mysql'); // mysql library
 const path = require('path')
 const config = require('config');// reference to config file
 const http = require('http');
+const bodyParser = require('body-parser'); // so we can read encoded forms ( necessary for form)
+const session = require('express-session');// so we can have session cookies for auth
+const { response } = require('express');
+
+
 
 const app = express();
 const PORT = 8080;
+
+const JsonParser = bodyParser.json();
+
+const urlEncodedParser = bodyParser.urlencoded({extended : false});// setting it to true will extend to all our REST methods
+
 
 const connection = mysql.createConnection({ // create connection
 
@@ -21,6 +31,16 @@ app.use(express.static('src'));// reference all static files
 
 app.use(express.json());
 
+app.set("views" , path.join(__dirname, '/src/views'));
+app.set('view engine', 'ejs');
+
+// TODO add password to config for now leave it as secret for test
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
 app.listen(
 
     PORT,
@@ -28,16 +48,56 @@ app.listen(
 
 )
 
-// base page
+//base page
 app.get('/',function(req,res) { // to open the main file 
-    res.sendFile( __dirname + '/src/html/todomain.html'); // adding the 
+    res.render('main');
 });
 
-// login page
-app.get('/login',function(req,res) { // to open the main file 
-    res.sendFile( __dirname + '/src/html/login.html'); // adding the 
+
+app.get('/authorised/:username', function(req,res) {
+
+
+    const {args} = req.params;
+
+    res.sendFile( path.join(__dirname , '/src/html/todomain.html')); // adding the 
+
+    console.log(args);
+
+})
+
+// login page called when clicked . how to call it from html ? should i make a request or 
+// can i leave it as href
+app.get('/auth',function(req,res) { // to open the main file 
+    res.sendFile( path.join(__dirname,'/src/html/login.html')); // adding the 
 });
 
+//TODO separate
+app.post('/auth', urlEncodedParser , (req,res) =>{
+
+    var username = req.body.username;
+    var password = req.body.password;
+
+    if(username && password){ // checking for empty fields
+        connection.query("SELECT * from todoapp.credentials p WHERE p.Username ="+ connection.escape(username) +"AND p.Password=" + connection.escape(password) , function(err,sqlResponse){// response from DB
+    
+            if(err) throw err;
+
+            if(sqlResponse.length > 0){ // at leats one matching password
+
+                req.session.loggedin = true;
+				req.session.username = username;
+                console.log("logged in as" + username);
+                res.render('main', {status : "logged in as "+ username});
+            }
+            res.end();
+        })
+    } 
+    else{
+        res.send('Please enter Username and Password');
+        res.end;
+    }
+
+})
 
 
 app.get('/product/:id' , (req, res) => {// response we send as get
